9f0d3617d9863b85db4c8b733e56506fef744a17
bash <sebabedo28@gmail.com>
2025-10-11 08:53:00 -0500

    feat(productos): añade módulo con mock local

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,15 @@ import HomePage from '../views/HomePage.vue'
 import LoginPage from '../views/LoginPage.vue'
 import RegisterPage from '../views/RegisterPage.vue'
 import { useSession } from '../composables/useSession'
+import ProductosListPage from '../views/productos/ProductosListPage.vue'
+import ProductosCreatePage from '../views/productos/ProductosCreatePage.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage, meta: { requiresAuth: true } },
   { path: '/inicio-sesion', name: 'Inicio-sesion', component: LoginPage, meta: { noLayout: true } },
   { path: '/registro', name: 'Registro', component: RegisterPage, meta: { noLayout: true } },
+  { path: '/productos', name: 'ProductosList', component: ProductosListPage,/*  meta: { requiresAuth: true } */ },
+  { path: '/productos/nuevo', name: 'ProductosCrear', component: ProductosCreatePage, /* meta: { requiresAuth: true } */ },
 ]
 
 const router = createRouter({
